e0fcf00c0f930262c3a95212a5f51716f22da75b
Boro Simon <simonboro810@gmail.com>
2023-09-18 05:03:18 -1200

    Commit last

--- Bassy_tresor_service/urls.py
+++ Bassy_tresor_service/urls.py
@@ -28,6 +28,6 @@ urlpatterns = [
     path('consultation/',include('consultation.urls')),
     path('communaute/',include('communaute.urls')),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-    # urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += staticfiles_urlpatterns()
